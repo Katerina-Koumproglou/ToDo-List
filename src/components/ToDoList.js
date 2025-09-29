bbd29c2d7d21fa3c2fb2ac5a27ebe506bd151165
Katerina Koumproglou <kat.koumproglou@gmail.com>
2025-09-29 14:49:07 +0300

    improved styling of popup calendar

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "./ToDoList.css";
 import notifSound from "../assets/sounds/notif-sound.mp3";
 
@@ -10,11 +12,13 @@ export const ToDoList = () => {
   const [tasks, setTasks] = useState([]);
   const [taskText, setTaskText] = useState("");
 
+  //Edit tasks
   const [editTask, setEditTask] = useState(null);
   const [editText, setEditText] = useState("");
 
   //Notification
   const [reminders, setReminders] = useState({});
+  const [showReminderInput, setShowReminderInput] = useState({});
 
   useEffect(() => {
     if ("Notification" in window && Notification.permission !== "granted") {
@@ -22,6 +26,7 @@ export const ToDoList = () => {
     }
   }, []);
 
+  //Add task button
   const handleAddButton = () => {
     const trimmedText = taskText.trim();
     if (!trimmedText) return;
@@ -65,13 +70,20 @@ export const ToDoList = () => {
 
   //Set reminder
   const handleSetReminder = (id, dateTime) => {
-    const reminderDate = new Date(dateTime);
-    if (reminderDate > new Date()) {
-      setReminders((prev) => ({ ...prev, [id]: reminderDate }));
+    if (dateTime && dateTime > new Date()) {
+      setReminders((prev) => ({ ...prev, [id]: dateTime }));
       toast.success("Reminder set!");
+      setShowReminderInput((prev) => ({ ...prev, [id]: false }));
     } else {
       toast.error("Please pick a future time.");
     }
+  };
+
+  const toggleReminderInput = (id) => {
+    setShowReminderInput((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
   };
 
   //Reminder goes off
@@ -120,6 +132,7 @@ export const ToDoList = () => {
           </button>
         </div>
         <ul>
+          {/* Creating checkbox for task: square icon for uncompleted / tick icon for completed */}
           {tasks.map((task) => (
             <li key={task.id}>
               <span
@@ -143,6 +156,7 @@ export const ToDoList = () => {
                 )}
               </span>
 
+              {/* Set edit button */}
               {editTask === task.id ? (
                 <>
                   <input
@@ -179,11 +193,33 @@ export const ToDoList = () => {
                   >
                     {task.text}
                   </span>
-                  <input
-                    type="datetime-local"
-                    className="reminder-input"
-                    onChange={(e) => handleSetReminder(task.id, e.target.value)}
-                  />
+
+                  {/* Reminder icon */}
+                  <span
+                    className="reminder-btn"
+                    onClick={() => toggleReminderInput(task.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 640 640"
+                    >
+                      <path
+                        fill="#f57a00"
+                        d="M568.4 196.5C563.9 207 550 206.3 543.5 196.9C515.7 156.9 477.4 124.7 432.5 104.3C422.1 99.6 418.8 86 428.4 79.7C443.4 69.8 461.4 64 480.7 64C533.3 64 575.9 106.6 575.9 159.2C575.9 172.4 573.2 185 568.3 196.5zM96.5 196.9C90 206.3 76 207 71.6 196.5C66.7 185 64 172.4 64 159.2C64 106.6 106.6 64 159.2 64C178.5 64 196.5 69.8 211.5 79.7C221.1 86 217.8 99.6 207.4 104.3C162.6 124.7 124.3 156.9 96.4 196.9zM454.2 531.4C416.8 559.4 370.3 576 320 576C269.7 576 223.2 559.4 185.9 531.4L150.6 566.6C138.1 579.1 117.8 579.1 105.3 566.6C92.8 554.1 92.8 533.8 105.3 521.3L140.5 486.1C112.6 448.8 96 402.3 96 352C96 228.3 196.3 128 320 128C443.7 128 544 228.3 544 352C544 402.3 527.4 448.8 499.4 486.2L534.6 521.4C547.1 533.9 547.1 554.2 534.6 566.7C522.1 579.2 501.8 579.2 489.3 566.7L454.1 531.5zM344 248C344 234.7 333.3 224 320 224C306.7 224 296 234.7 296 248L296 352C296 358.4 298.5 364.5 303 369L359 425C368.4 434.4 383.6 434.4 392.9 425C402.2 415.6 402.3 400.4 392.9 391.1L343.9 342.1L343.9 248z"
+                      />
+                    </svg>
+                  </span>
+                  {/* Only show calendar when toggled */}
+                  {showReminderInput[task.id] && (
+                    <DatePicker
+                      selected={reminders[task.id] || null}
+                      onChange={(date) => handleSetReminder(task.id, date)}
+                      showTimeSelect
+                      timeIntervals={1}
+                      dateFormat="Pp"
+                      withPortal
+                    />
+                  )}
 
                   <span
                     className="edit-btn"
