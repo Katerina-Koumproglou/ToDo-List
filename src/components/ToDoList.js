6c6e74ee27c070a5239c627625d3c5a0bac9c264
Katerina Koumproglou <kat.koumproglou@gmail.com>
2025-09-29 04:43:03 +0300

    added edit and delete function

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -7,6 +7,9 @@ export const ToDoList = () => {
   const [tasks, setTasks] = useState([]);
   const [taskText, setTaskText] = useState("");
 
+  const [editTask, setEditTask] = useState(null);
+  const [editText, setEditText] = useState("");
+
   const handleAddButton = () => {
     const trimmedText = taskText.trim();
     if (!trimmedText) return;
@@ -21,18 +24,31 @@ export const ToDoList = () => {
     setTaskText("");
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      handleAddButton();
-    }
-  };
-
   const toggleComplete = (id) => {
     setTasks((prev) =>
       prev.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
       )
     );
+  };
+
+  //Edit function
+  const startEdit = (id, text) => {
+    setEditTask(id);
+    setEditText(text);
+  };
+
+  const saveEdit = (id) => {
+    setTasks((prev) =>
+      prev.map((task) => (task.id === id ? { ...task, text: editText } : task))
+    );
+    setEditTask(null);
+    setEditText("");
+  };
+
+  //Delete function
+  const deleteTask = (id) => {
+    setTasks((prev) => prev.filter((task) => task.id !== id));
   };
 
   return (
@@ -45,7 +61,9 @@ export const ToDoList = () => {
             placeholder="Enter your task..."
             value={taskText}
             onChange={(e) => setTaskText(e.target.value)}
-            onKeyDown={handleKeyDown}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") handleAddButton();
+            }}
           />
           <button className="add-btn" onClick={handleAddButton}>
             ADD
@@ -74,11 +92,75 @@ export const ToDoList = () => {
                   </svg>
                 )}
               </span>
-              <span
-                className={`task-completed ${task.completed ? "checked" : ""}`}
-              >
-                {task.text}
-              </span>
+
+              {editTask === task.id ? (
+                <>
+                  <input
+                    className={`task-edit-input ${
+                      editTask === task.id ? "editing" : ""
+                    }`}
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") saveEdit(task.id);
+                    }}
+                  />
+                  <span
+                    className="edit-btn"
+                    onClick={() => saveEdit(task.id, task.text)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 640 640"
+                    >
+                      <path
+                        fill="#f57a00"
+                        d="M480 96C515.3 96 544 124.7 544 160L544 480C544 515.3 515.3 544 480 544L160 544C124.7 544 96 515.3 96 480L96 160C96 124.7 124.7 96 160 96L480 96zM160 144C151.2 144 144 151.2 144 160L144 480C144 488.8 151.2 496 160 496L480 496C488.8 496 496 488.8 496 480L496 160C496 151.2 488.8 144 480 144L160 144zM390.7 233.9C398.5 223.2 413.5 220.8 424.2 228.6C434.9 236.4 437.3 251.4 429.5 262.1L307.4 430.1C303.3 435.8 296.9 439.4 289.9 439.9C282.9 440.4 276 437.9 271.1 433L215.2 377.1C205.8 367.7 205.8 352.5 215.2 343.2C224.6 333.9 239.8 333.8 249.1 343.2L285.1 379.2L390.7 234z"
+                      />
+                    </svg>
+                  </span>
+                </>
+              ) : (
+                <>
+                  <span
+                    className={`task-completed ${
+                      task.completed ? "checked" : ""
+                    }`}
+                  >
+                    {task.text}
+                  </span>
+                  <span
+                    className="edit-btn"
+                    onClick={() => startEdit(task.id, task.text)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 640 640"
+                    >
+                      <path
+                        fill="#f57a00"
+                        d="M505 122.9L517.1 135C526.5 144.4 526.5 159.6 517.1 168.9L488 198.1L441.9 152L471 122.9C480.4 113.5 495.6 113.5 504.9 122.9zM273.8 320.2L408 185.9L454.1 232L319.8 366.2C316.9 369.1 313.3 371.2 309.4 372.3L250.9 389L267.6 330.5C268.7 326.6 270.8 323 273.7 320.1zM437.1 89L239.8 286.2C231.1 294.9 224.8 305.6 221.5 317.3L192.9 417.3C190.5 425.7 192.8 434.7 199 440.9C205.2 447.1 214.2 449.4 222.6 447L322.6 418.4C334.4 415 345.1 408.7 353.7 400.1L551 202.9C579.1 174.8 579.1 129.2 551 101.1L538.9 89C510.8 60.9 465.2 60.9 437.1 89zM152 128C103.4 128 64 167.4 64 216L64 488C64 536.6 103.4 576 152 576L424 576C472.6 576 512 536.6 512 488L512 376C512 362.7 501.3 352 488 352C474.7 352 464 362.7 464 376L464 488C464 510.1 446.1 528 424 528L152 528C129.9 528 112 510.1 112 488L112 216C112 193.9 129.9 176 152 176L264 176C277.3 176 288 165.3 288 152C288 138.7 277.3 128 264 128L152 128z"
+                      />
+                    </svg>
+                  </span>
+
+                  {/* Delete Button */}
+                  <span
+                    className="delete-btn"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 640 640"
+                    >
+                      <path
+                        fill="#f57a00"
+                        d="M262.2 48C248.9 48 236.9 56.3 232.2 68.8L216 112L120 112C106.7 112 96 122.7 96 136C96 149.3 106.7 160 120 160L520 160C533.3 160 544 149.3 544 136C544 122.7 533.3 112 520 112L424 112L407.8 68.8C403.1 56.3 391.2 48 377.8 48L262.2 48zM128 208L128 512C128 547.3 156.7 576 192 576L448 576C483.3 576 512 547.3 512 512L512 208L464 208L464 512C464 520.8 456.8 528 448 528L192 528C183.2 528 176 520.8 176 512L176 208L128 208zM288 280C288 266.7 277.3 256 264 256C250.7 256 240 266.7 240 280L240 456C240 469.3 250.7 480 264 480C277.3 480 288 469.3 288 456L288 280zM400 280C400 266.7 389.3 256 376 256C362.7 256 352 266.7 352 280L352 456C352 469.3 362.7 480 376 480C389.3 480 400 469.3 400 456L400 280z"
+                      />
+                    </svg>
+                  </span>
+                </>
+              )}
             </li>
           ))}
         </ul>
